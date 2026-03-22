5611a3df838b53e357a8b6d4c783a61bd552f1b7
2503dhruv <dhruvsharma9591@gmail.com>
2026-03-22 18:44:32 +0530

    error found

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,14 +11,20 @@ import slideRoutes from "./routes/slides.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "http://localhost:3000",
-  "https://freelancing-seven.vercel.app",
-  "https://admin-seven-orcin-50.vercel.app/",
-],
-credentials: true,}));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:3000",
+    "https://freelancing-seven.vercel.app",
+    "https://admin-seven-orcin-50.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
+
+app.options("*", cors());
+
 app.use(express.json());
 
 mongoose
