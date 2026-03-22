3f9c5a1a77a63113c704b5782069126dffc922dd
2503dhruv <dhruvsharma9591@gmail.com>
2026-03-22 18:30:19 +0530

    index.js changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,6 +16,7 @@ app.use(cors({ origin: [
   "http://localhost:5174",
   "http://localhost:3000",
   "https://freelancing-seven.vercel.app",
+  "https://admin-seven-orcin-50.vercel.app/",
 ],
 credentials: true,}));
 app.use(express.json());
